5ddebc4268f1f3c634b0a7e3b61b648aad4433f7
Eugene Serikoff <mail.serikoff@gmail.com>
2018-05-29 23:22:22 +0300

    Changes 29.05.2019 v2

--- JavaShieldt/src/Box.java
+++ JavaShieldt/src/Box.java
@@ -4,6 +4,12 @@ public class Box {
     double height;
     double depth;
 
+    public Box(double weidth, double height, double depth) {
+        this.weidth = weidth;
+        this.height = height;
+        this.depth = depth;
+    }
+
     //Return volume
     double volume(){
         return weidth * height * depth;
--- JavaShieldt/src/Main.java
+++ JavaShieldt/src/Main.java
@@ -2,18 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Box myBox1 = new Box();
-        Box myBox2 = new Box();
+        Box myBox1 = new Box(10, 20, 15);
+        Box myBox2 = new Box(3, 6, 9);
 
         double vol;
-
-        myBox1.weidth = 10;
-        myBox1.height = 20;
-        myBox1.depth = 15;
-
-        myBox2.weidth = 3;
-        myBox2.height = 6;
-        myBox2.depth = 9;
 
         vol = myBox1.volume();
         System.out.println("Volume: " + vol);
